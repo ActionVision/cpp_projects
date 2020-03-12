1c87f4d76c9dfa077cc67a4527493a5649d8fc8e
ActionVision <wgh19911214@126.com>
2020-03-12 21:14:08 +0800

    add other map functions

--- boostTest/boostTest/boostTest.cpp
+++ boostTest/boostTest/boostTest.cpp
@@ -46,7 +46,8 @@ void test_json()
 		vecStr.push_back(stra);
 	}
 }
-void test_map()
+//数据的插入，第一种方式：用insert函数插入pair数据
+void test_map1()
 {
 	map<int, string> mapStudent;
 	mapStudent.insert(pair<int, string>(1, "student1"));
@@ -54,20 +55,127 @@ void test_map()
 	mapStudent.insert(pair<int, string>(3, "student3"));
 	mapStudent.insert(pair<int, string>(4, "student4"));
 	mapStudent.insert(pair<int, string>(5, "student5"));
+	int nSize = mapStudent.size();
 
 	map<int, string>::iterator iter;
 	for (iter = mapStudent.begin();iter != mapStudent.end();iter++)
 	{
-		cout << iter->first << "" << iter->second << endl;
+		cout << iter->first << " " << iter->second << endl;
 
 	}
 }
+//第二种方式，用insert函数插入value_type数据，
+void test_map2()
+{
+	map<int, string> mapStudent;
+	mapStudent.insert(map<int, string>::value_type(1, "student1"));
+	mapStudent.insert(map<int, string>::value_type(2, "student2"));
+	mapStudent.insert(map<int, string>::value_type(3, "student3"));
+	mapStudent.insert(map<int, string>::value_type(4, "student4"));
+	mapStudent.insert(map<int, string>::value_type(5, "student5"));
+	int nSize = mapStudent.size();
+
+	map<int, string>::iterator iter;
+	for (iter = mapStudent.begin();iter != mapStudent.end();iter++)
+	{
+		cout << iter->first << " " << iter->second << endl;
+
+	}
+
+}
+// map删除数据
+void test_map3()
+{
+	map<int, string>mapStudent;
+	mapStudent[1] = "student1";
+	mapStudent[2] = "student2";
+	mapStudent[3] = "student3";
+	mapStudent[4] = "student4";
+	mapStudent[5] = "student5";
+	int nSize = mapStudent.size();
+
+	map<int, string>::iterator iter;
+	for (iter = mapStudent.begin();iter != mapStudent.end();iter++)
+	{
+		cout << iter->first << " " << iter->second << endl;
+	}
+	//数组删除
+	iter = mapStudent.find(3);
+	mapStudent.erase(iter);
+	cout << "\ndelete somethings" << endl;
+	for (iter = mapStudent.begin();iter != mapStudent.end();iter++)
+	{
+		cout << iter->first << " " << iter->second << endl;
+	}
+}
+typedef struct tagStudentinfo
+{
+	int      niD;
+	string   strName;
+	bool operator < (tagStudentinfo const& _A) const
+	{     //这个函数指定排序策略，按niD排序，如果niD相等的话，按strName排序  
+
+		if (niD < _A.niD) return true;
+		if (niD == _A.niD)
+			return strName.compare(_A.strName) < 0;
+		return false;
+	}
+}Studentinfo, *PStudentinfo; //学生信息 
+void test_map4()
+{
+	int  nSize;//用学生信息映射分数
+	map<Studentinfo, int>mapStudent;
+	map<Studentinfo, int>::iterator iter;
+	Studentinfo studentinfo;
+	studentinfo.niD = 1;
+	studentinfo.strName = "student1";
+	mapStudent.insert(pair<Studentinfo, int>(studentinfo, 90));
+	studentinfo.niD = 2;
+	studentinfo.strName = "student_two";
+	mapStudent.insert(pair<Studentinfo, int>(studentinfo, 80));
+	for (iter = mapStudent.begin(); iter != mapStudent.end(); iter++)
+
+		cout << iter->first.niD << ' ' << iter->first.strName << ' ' << iter->second << endl;
+
+}
+//map的基本函数
+//begin(),clear(),count(),empty,end,equal_range,erase,find,insert,
+void test_map5()
+{
+	map<int, string>mapStudent;
+	mapStudent[1] = "student1";
+	mapStudent[2] = "student2";
+	mapStudent[3] = "student3";
+	mapStudent[4] = "student4";
+	mapStudent[5] = "student5";
+	//元素的个数
+	int nSize = mapStudent.size();
+
+	map<int, string>::iterator iter;
+	
+	//查找&删除
+	iter = mapStudent.find(3);
+	mapStudent.erase(iter);
+	//插入
+	mapStudent.insert(pair<int, string>(9, "student9"));
+	mapStudent.insert(pair<int, string>(9, "student9"));
+	//获取某个元素的个数
+	int num = mapStudent.count(9);
+	cout << "num 9个数为"<<num<<endl;
+	//清理掉所有的数据
+	mapStudent.clear();
+}
+
 
 int main()
 {
 	//test_thread();
 	//test_json();
-	test_map();
+	//test_map1();
+	//test_map2();
+	//test_map3();
+	//test_map4();
+	test_map5();
 	return 0;
 }
 
